4535355e3ef6dedb05c7da218b6b6bf58d21010e
evs <evoraleite@gmail.com>
2019-08-21 13:16:11 -0300

    first commit: cipher.js ok, index.js ok, html implementing tabs, contact and about needing content, style.css ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,43 @@
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode
 };
+
+let offsetEncode;
+let textEncode;
+
+function encode(offset, string){
+	event.preventDefault();
+
+	let char;
+	let resultCharCode;
+	let resultText = "";
+
+	for (let i=0; i < textEncode.length; i++){
+		char = textEncode[i].charCodeAt();
+		
+		resultCharCode = ((char + offsetEncode - 32)%95)+32;
+		resultText += String.fromCharCode(resultCharCode);
+	}
+	return resultText;
+}
+
+let offsetDecode;
+let textDecode;
+
+function decode(offset, string){
+	event.preventDefault();
+
+	let char;
+	let resultCharCode;
+	let resultText = "";
+
+	for (let i=0; i < textDecode.length; i++){
+		char = textDecode[i].charCodeAt();
+
+		resultCharCode = (((char - offsetDecode)-32)%95)+32;
+		resultText += String.fromCharCode(resultCharCode);
+	}
+	return resultText;
+}
+
---
+++ src/index.js
@@ -0,0 +1,22 @@
+//funções de cliques, eventos do DOM
+function functionEncode(){
+	event.preventDefault();
+	offsetEncode = Number(document.getElementById('number-offset').value);
+	textEncode = document.getElementById('text').value;
+
+	let resultText = encode(offsetEncode, textEncode);
+
+	document.getElementById('cipher-text-result').innerHTML = resultText;
+}
+//------------------------------------------------------------------------------------------------//
+function functionDecode(){
+	event.preventDefault();
+	offsetDecode = Number(document.getElementById('offset-decipher').value);
+	textDecode = document.getElementById('text-decipher').value;
+
+	let resultText = decode(offsetDecode, textDecode);
+
+	document.getElementById('decipher-text-result').innerHTML = resultText;
+}
+
+
