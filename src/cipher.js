22f3e3d29952be2c8eb2ac8668438853766511a8
evs <evoraleite@gmail.com>
2019-08-22 13:51:23 -0300

    changed tabs to spaces in index.js and cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -6,38 +6,38 @@ window.cipher = {
 let offsetEncode;
 let textEncode;
 
-function encode(offset, string){
-	event.preventDefault();
+function encode(offset, string) {
+  event.preventDefault();
 
-	let char;
-	let resultCharCode;
-	let resultText = "";
+  let char;
+  let resultCharCode;
+  let resultText = "";
 
-	for (let i=0; i < textEncode.length; i++){
-		char = textEncode[i].charCodeAt();
+  for (let i=0; i < textEncode.length; i++) {
+    char = textEncode[i].charCodeAt();
 		
-		resultCharCode = ((char + offsetEncode - 32)%95)+32;
-		resultText += String.fromCharCode(resultCharCode);
-	}
-	return resultText;
+    resultCharCode = ((char + offsetEncode - 32)%95)+32;
+    resultText += String.fromCharCode(resultCharCode);
+  }
+  return resultText;
 }
 
 let offsetDecode;
 let textDecode;
 
-function decode(offset, string){
-	event.preventDefault();
+function decode(offset, string) {
+  event.preventDefault();
 
-	let char;
-	let resultCharCode;
-	let resultText = "";
+  let char;
+  let resultCharCode;
+  let resultText = "";
 
-	for (let i=0; i < textDecode.length; i++){
-		char = textDecode[i].charCodeAt();
+  for (let i=0; i < textDecode.length; i++) {
+    char = textDecode[i].charCodeAt();
 
-		resultCharCode = (((char - offsetDecode)-32)%95)+32;
-		resultText += String.fromCharCode(resultCharCode);
-	}
-	return resultText;
+    resultCharCode = (((char - offsetDecode)-32)%95)+32;
+    resultText += String.fromCharCode(resultCharCode);
+  }
+  return resultText;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 //funções de cliques, eventos do DOM
-function functionEncode(){
-	event.preventDefault();
-	offsetEncode = Number(document.getElementById('number-offset').value);
-	textEncode = document.getElementById('text').value;
+function functionEncode() {
+  event.preventDefault();
+  offsetEncode = Number(document.getElementById("number-offset").value);
+  textEncode = document.getElementById("text").value;
 
-	let resultText = encode(offsetEncode, textEncode);
+  let resultText = encode(offsetEncode, textEncode);
 
-	document.getElementById('cipher-text-result').innerHTML = resultText;
+  document.getElementById("cipher-text-result").innerHTML = resultText;
 }
 //------------------------------------------------------------------------------------------------//
-function functionDecode(){
-	event.preventDefault();
-	offsetDecode = Number(document.getElementById('offset-decipher').value);
-	textDecode = document.getElementById('text-decipher').value;
+function functionDecode() {
+  event.preventDefault();
+  offsetDecode = Number(document.getElementById("offset-decipher").value);
+  textDecode = document.getElementById("text-decipher").value;
+  
+  let resultText = decode(offsetDecode, textDecode);
 
-	let resultText = decode(offsetDecode, textDecode);
-
-	document.getElementById('decipher-text-result').innerHTML = resultText;
+  document.getElementById("decipher-text-result").innerHTML = resultText;
 }
-
-
 // TESTE
+function selectTab(tabIndex) {
+  document.getElementById("tab1Content").style.display = "none";
+  document.getElementById("tab2Content").style.display = "none";
 
-function selectTab(tabIndex){
-	document.getElementById('tab1Content').style.display = 'none';
-	document.getElementById('tab2Content').style.display = 'none';
-
-	document.getElementById('tab' + tabIndex + 'Content').style.display = 'block';
+  document.getElementById("tab" + tabIndex + "Content").style.display = "block";
 }
 
